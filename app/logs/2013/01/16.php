c661ed551bfb080b951040c27e20c54df15edb48
Jlake Ou <ouzhiwei@gmail.com>
2013-07-09 18:33:17 +0900

    delete log files

--- app/logs/2013/01/16.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Warning - 2013-01-16 16:39:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2013-01-16 17:35:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
